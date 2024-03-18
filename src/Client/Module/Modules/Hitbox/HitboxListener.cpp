d3c496fa44cc3df128bdbdd3579b4123b65a19a5
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2024-03-18 15:36:35 +0100

    Update HitboxListener.cpp

--- src/Client/Module/Modules/Hitbox/HitboxListener.cpp
+++ src/Client/Module/Modules/Hitbox/HitboxListener.cpp
@@ -1,5 +1,7 @@
 #include "HitboxListener.hpp"
 
+#include <cmath>
+
 #include "../../Manager.hpp"
 #include "../../../../Utils/Render/DrawUtils.hpp"
 
@@ -10,13 +12,10 @@ void renderBox(Player* player) {
 	if (box->settings.getSettingByName<bool>("color_rgb")->value) color2 = MusuiGUI::rgbColor;
 	else color2 = MusuiGUI::HexToColorF(box->settings.getSettingByName<std::string>("color")->value);
 	auto localPlayer = SDK::clientInstance->getLocalPlayer();
-    float dist = localPlayer->getPosition()->dist(*player->getPosition());
-    if(player == nullptr) return;
-	// This may let through some entites
-	if (player == localPlayer || !player || !player->isAlive() || !localPlayer->isValidTarget(player) || !HitboxListener::canSeeArrXD[player->getNametag()] || dist > 30)
-		return;
+	if (player == localPlayer || !player || !player->isAlive() || !localPlayer->isValidTarget(player)) return;
 
-    DrawUtils::addEntityBox(player, (float)fmax(0.5f, 1 / (float)fmax(1, localPlayer->getRenderPositionComponent()->renderPos.dist(player->getRenderPositionComponent()->renderPos))), color2);}
+    auto value = (float)std::fmax(0.5f, 1 / (float)fmax(1, localPlayer->getRenderPositionComponent()->renderPos.dist(player->getRenderPositionComponent()->renderPos)));
+    DrawUtils::addEntityBox(player, value, color2);}
 
 void HitboxListener::onRender(RenderEvent& event) {
 	if (!this->module->settings.getSettingByName<bool>("enabled")->value)
@@ -29,8 +28,9 @@ void HitboxListener::onRender(RenderEvent& event) {
 
     if(player != nullptr) {
         for (const auto& ent: player->level->getRuntimeActorList()) {
-            if (ent != nullptr && ent->isPlayer() && ent->hasCategory(ActorCategory::Player))
-                renderBox((Player*)ent);
+            if (ent != nullptr && ent->isPlayer() && ent->hasCategory(ActorCategory::Player)) {
+                renderBox((Player *) ent);
+            }
         }
     }
 }
